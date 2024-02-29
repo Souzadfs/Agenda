b850a899bc201805ed693b17510b407ea95db65d
Souzadfs <souzadf09@gmail.com>
2024-02-29 20:43:14 -0300

    Corrigindo HTML

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,12 @@
+# from django.shortcuts import render
+
+
+# def index(request):
+#     return render(
+#         request,
+#         'contact/index.html',
+#     )
+
 from django.shortcuts import render
 
 
