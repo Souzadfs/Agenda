b06bd867508cab396a74c26a552eb8e2854c0384
Souzadfs <souzadf09@gmail.com>
2024-03-02 20:29:14 -0300

    criando os models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+# key - id (primary automatic)
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date(data), description (text),
+# category (foreing key), show(boolean), owner (foreing key)
+# picture.
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Contact(models.model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max length=20)
+    email = models.EmailField(max length=254)
+    created_date = models.DateTimeField(defaut = timezone)
+
