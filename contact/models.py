f09ef37e071a4c546cc3ee49161ed9808296a856
Souzadfs <souzadf09@gmail.com>
2024-03-03 21:23:56 -0300

    Recriando Models

--- contact/models.py
+++ contact/models.py
@@ -3,14 +3,18 @@
 # email (email), created_date(data), description (text),
 # category (foreing key), show(boolean), owner (foreing key)
 # picture.
-
 from django.db import models
 from django.utils import timezone
 
 class Contact(models.model):
+    # first_name = models.CharField(max_length=50)
+    # last_name = models.CharField(max_length=50)
+    # phone = models.CharField(max_length=50)
+    # email = models.EmailField(max_length=254)
+    # created_date = models.DateTimeField(defaut=timezone)
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    phone = models.CharField(max length=20)
-    email = models.EmailField(max length=254)
-    created_date = models.DateTimeField(defaut = timezone)
-
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
