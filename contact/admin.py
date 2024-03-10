34ab32aab5b81b9ba5f08de21afcf10caeb98083
Souzadfs <souzadf09@gmail.com>
2024-03-10 20:09:52 -0300

    add the firter and search fields

--- contact/admin.py
+++ contact/admin.py
@@ -4,8 +4,10 @@ from contact import models
 @admin.register(models.Contact)
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'first_name','last_name','phone',
+    list_display = 'id','first_name','last_name','phone',
     ordering = 'id',
+    list_filter = 'first_name','last_name','phone',
+    search_fields = 'first_name','last_name',
 
 
 # Register your models here.
