de37d1d6e54ad9300971ec0820dc2a9062e3c50c
Souzadfs <souzadf09@gmail.com>
2024-03-10 19:57:36 -0300

    Desenvolvendo o ADMIN

--- contact/admin.py
+++ contact/admin.py
@@ -4,6 +4,7 @@ from contact import models
 @admin.register(models.Contact)
 
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'first_name','last_name','phone',
+
 
 # Register your models here.
