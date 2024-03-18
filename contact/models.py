416cd1036d27591d19e586a1f804bf5e1a5ca9a6
Souzadfs <souzadf09@gmail.com>
2024-03-18 20:21:37 -0300

    Criando category

--- contact/models.py
+++ contact/models.py
@@ -6,6 +6,9 @@
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
